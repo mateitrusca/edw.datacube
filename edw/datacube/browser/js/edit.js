ecbbcfeceb1a74671de77787986511cf25fd25ac
David Batranu <dbatranu@gmail.com>
2013-03-21 15:26:55 +0200

    Fixing layout, js and setup.py

--- edw/datacube/browser/js/edit.js
+++ edw/datacube/browser/js/edit.js
@@ -34,18 +34,14 @@ scoreboard.datacube.edit = {
         var data = [
             {'uri': 'some-dataset-uri-1',
              'title': 'Some dataset 1'},
-            {'uri': 'some-dataset-uri-2'
+            {'uri': 'some-dataset-uri-2',
              'title': 'Some dataset 2'},
-            },
-            {'uri': 'some-dataset-uri-3'
+            {'uri': 'some-dataset-uri-3',
              'title': 'Some dataset 3'},
-            },
-            {'uri': 'some-dataset-uri-4'
+            {'uri': 'some-dataset-uri-4',
              'title': 'Some dataset 4'},
-            },
-            {'uri': 'some-dataset-uri-5'
-             'title': 'Some dataset 5'},
-            },
+            {'uri': 'some-dataset-uri-5',
+             'title': 'Some dataset 5'}
         ];
         return data
     },
