a865cc3592fcf9a363a774a8b82eeb37a24a4865
Alex Morega <alex@grep.ro>
2013-05-07 16:34:50 +0300

    include gouped dimensions in notations query

--- edw/datacube/data/tests/notations_test.py
+++ edw/datacube/data/tests/notations_test.py
@@ -10,3 +10,15 @@ def test_notations_lookup_finds_values():
     assert (cube.notations.lookup_notation('indicator', 'bb_lines')['uri']
             == bb_lines_uri)
     assert cube.notations.lookup_uri(bb_lines_uri)['notation'] == 'bb_lines'
+
+
+@sparql_test
+def test_notations_lookup_finds_dimension_groups():
+    cube = create_cube()
+    ns_and_notation = ('indicator-group', 'internet-services')
+    internet_services_uri = ('http://semantic.digital-agenda-data.eu/'
+                             'codelist/indicator-group/internet-services')
+    assert (cube.notations.lookup_notation(*ns_and_notation)['uri']
+            == internet_services_uri)
+    assert (cube.notations.lookup_uri(internet_services_uri)['notation']
+            == 'internet-services')
